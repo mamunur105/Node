bde3bcf1faec5c9f48c0920d4fcd3e6271e75854
Mamunur Rashid <30565480+mamunur105@users.noreply.github.com>
2021-05-31 01:55:57 +0600

    Understanding Response Object

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ https://scotch.io/bar-talk/10-node-frameworks-to-use-in-2019
 
 
 
-https://www.youtube.com/watch?v=jxQdFmW69GA&list=PL_XxuZqN0xVDHFj-ecFSU0SU-B0TuJRk9&index=27
+https://www.youtube.com/watch?v=8w0lcdmwnNg&list=PL_XxuZqN0xVDHFj-ecFSU0SU-B0TuJRk9&index=37
--- expressjs/app.js
+++ expressjs/app.js
@@ -1,27 +1,23 @@
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 8080
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8080;
 
+// Route
 
-// Route 
 app.get('/about', (req, res) => {
-	res.send('Welcome to about page!')
-})
-
-
-
-
-
-
-
-
-
+	res.send('Welcome to about page!');
+});
+app.get('/json', (req, res) => {
+	res.json({
+		message: 'I am response'
+	});
+});
 app.get('/', (req, res) => {
-	res.send('Hello World!')
-})
+	res.send('Hello World!');
+});
 app.get('*', (req, res) => {
-	res.send('<h1>404 Not Found</h1>')
-})
-app.listen(PORT , () => {
-	console.log(`Server Is running on port ${PORT}`)
-})
+	res.send('<h1>404 Not Found</h1>');
+});
+app.listen(PORT, () => {
+	console.log(`Server Is running on port ${PORT}`);
+});
