c63dd44a46c0301b0c78ae550d426bc5d658ea06
Mamunur Rashid <30565480+mamunur105@users.noreply.github.com>
2021-06-10 01:51:27 +0600

    Auth controller and Partial header footer

--- README.md
+++ README.md
@@ -3,4 +3,5 @@ https://scotch.io/bar-talk/10-node-frameworks-to-use-in-2019
 
 
 
-https://www.youtube.com/watch?v=r8wEZpq9DE8&list=PL_XxuZqN0xVDHFj-ecFSU0SU-B0TuJRk9&index=102
+
+https://www.youtube.com/watch?v=Lr8YN15k4PU&list=PL_XxuZqN0xVDHFj-ecFSU0SU-B0TuJRk9&index=106
--- mvc/app.js
+++ mvc/app.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const morgan = require('morgan');
-// getting-started.js
-const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
-
 app.set( 'view engine', 'ejs')
+app.set( 'views', 'views')
 
-app.use( morgan('dev') )
-app.use( express.urlencoded( {extended: true} ) )
-app.use( express.json() )
-
+const middleware = [
+	 morgan('dev'),
+	 express.static('public'),
+	 express.urlencoded( {extended: true} ),
+	 express.json()
+]
+app.use( middleware )
 
 // User router start
 app.get('/', (req, res) => {
-		res.send('Home');
+	res.render('pages/auth/signup', { title: 'Create A New Account' });
 });
 app.get('*', (req, res) => {
 	res.send('<h1>404 Not Found</h1>');
